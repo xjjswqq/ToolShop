1ae7a2e16b14beeb9051d4ae0c593dcbd9d1ab7d
xjjswqq <hpkkoledjmiilubimii@gmail.com>
2024-10-08 22:03:02 +0300

    SCRUM-3 added migrations

---
+++ database/migrations/2024_10_08_174443_create_orders_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('customer_id'); // Змінено на customer_id
+            $table->decimal('total_amount', 10, 2);
+            $table->string('status')->default('pending');
+            $table->timestamps();
+
+            // Зовнішній ключ
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
